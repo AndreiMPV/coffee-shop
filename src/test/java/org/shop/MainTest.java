4cd0966f043293b8ba70d80fb81c0a61b2a94d40
Andrei M <andreim.epam@navan.tech>
2024-08-20 12:55:40 +0200

    Initialize project

---
+++ src/test/java/org/shop/MainTest.java
@@ -0,0 +1,14 @@
+package org.shop;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class MainTest {
+
+    @Test
+    void test() {
+        assertTrue(true);
+    }
+}
